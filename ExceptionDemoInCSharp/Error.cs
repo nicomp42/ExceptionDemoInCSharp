55b365fa765490ecb9fe16de167a3cc0b8321647
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-11-06 14:48:33 -0500

    Added a custom exception object

--- ExceptionDemoInCSharp/Error.cs
+++ ExceptionDemoInCSharp/Error.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/***********************************************
+ * There is a flaw in the design of this class
+ * *********************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +14,16 @@ namespace ExceptionDemoInCSharp
     /// </summary>
     class Error
     {
-        private String message { get; set; }
+        private String mMessage;
+        private String message
+        {
+            get { return mMessage; }
+            set {
+                // Throw an exception if the message is blank and do not store the message
+                if (value.Trim().Length == 0) { throw new ErrorMessageException("message length cannot be zero"); }
+                mMessage = value;
+            }
+        }
         public Error(String message)
         {
             this.message = message;
---
+++ ExceptionDemoInCSharp/ErrorMessageException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemoInCSharp
+{
+    class ErrorMessageException :Exception
+    {
+        public ErrorMessageException(String message) : base (message)
+        {}
+    }
+}
